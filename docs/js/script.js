68d79397276f2036b46af85369d3ba599b456e03
mmerezhko <mmere@softserveinc.com>
2023-08-14 17:26:37 +0300

    Update work experience

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,12 +1,15 @@
 const timeSince = () => {
   const today = new Date();
   const firstDay = new Date(2018, 9, 1);
+  const daysDiff = Math.ceil(
+    Math.abs(today - firstDay) / (1000 * 60 * 60 * 24)
+  );
+  const years = Math.floor(daysDiff / 365.25);
+  const remainingDays = Math.floor(daysDiff - years * 365.25);
+  const months = Math.floor((remainingDays / 365.25) * 12);
+  const days = Math.ceil(daysDiff - (years * 365.25 + (months / 12) * 365.25));
 
-  let diff = (today.getTime() - firstDay.getTime()) / 1000;
-
-  diff /= 60 * 60 * 24 * 7 * 4;
-
-  return `${Math.abs(Math.round(diff))} months`;
+  return `${years} years ${months} months ${days} days`;
 };
 
-document.getElementById("exp").innerText = timeSince();
+document.getElementById('exp').innerText = timeSince();
