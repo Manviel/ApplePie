d0721b6bdc79da2e8af46f7c27ff1ffb18e727e7
Manviel <mmere@softserveinc.com>
2026-01-09 12:12:39 +0200

    Update format for duty list

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,15 +1,18 @@
 const timeSince = () => {
+  const leapYear = 365.25;
   const today = new Date();
   const firstDay = new Date(2018, 9, 1);
   const daysDiff = Math.ceil(
     Math.abs(today - firstDay) / (1000 * 60 * 60 * 24)
   );
-  const years = Math.floor(daysDiff / 365.25);
-  const remainingDays = Math.floor(daysDiff - years * 365.25);
-  const months = Math.floor((remainingDays / 365.25) * 12);
-  const days = Math.ceil(daysDiff - (years * 365.25 + (months / 12) * 365.25));
+  const years = Math.floor(daysDiff / leapYear);
+  const remainingDays = Math.floor(daysDiff - years * leapYear);
+  const months = Math.floor((remainingDays / leapYear) * 12);
+  const days = Math.ceil(
+    daysDiff - (years * leapYear + (months / 12) * leapYear)
+  );
 
   return `${years} years ${months} months ${days} days`;
 };
 
-document.getElementById('exp').innerText = timeSince();
+document.getElementById("exp").innerText = timeSince();
